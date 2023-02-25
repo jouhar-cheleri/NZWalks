7676319cb8ecb0f28b08b96c19c6888e92a15d26
jouhar-cheleri <jouharnift@gmail.com>
2023-02-25 17:10:44 +0530

    End of Session 2

---
+++ NZWalks/NZWalks.API/Db/NZWalkDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Model.Domain;
+
+namespace NZWalks.API.Db
+{
+    public class NZWalkDbContext : DbContext
+    {
+        public NZWalkDbContext(DbContextOptions<NZWalkDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDiffculty> WalkDiffculty { get; set; }
+
+    }
+}
---
+++ NZWalks/NZWalks.API/Model/Domain/WalkDiffculty.cs
@@ -0,0 +1,8 @@
+﻿namespace NZWalks.API.Model.Domain
+{
+    public class WalkDiffculty
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+    }
+}
